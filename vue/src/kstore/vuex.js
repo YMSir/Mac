9c0e0741cbde041be5c851b86c1336cdc04e83c4
mac <yes_man_yu@163.com>
2021-07-13 06:10:36 +0800

    vue vuex 20210713

--- vue/src/kstore/vuex.js
+++ vue/src/kstore/vuex.js
@@ -1,4 +1,22 @@
 /**
  * Created by Yes.Man on 2021/7/12 23:44.
- * file: vuex
+ * file: Vuex
  */
+
+export class Store {
+  constructor (options) {
+
+  }
+
+  commit () {
+
+  }
+
+  dispatch () {
+
+  }
+}
+
+export function install (Vue) {
+
+}

24d7bc72b60e17ff50d6c06839a7bec863e9d108
Yes.Man <yes_man_yu@163.com>
2021-07-31 15:43:20 +0800

    vue-next

---
+++ vue-next-master/packages/vue/examples/test/proxy.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Yes.Man on 2021/7/31 07:31.
+ * @file: proxy
+ */
+
+/*-----------------*/
+/*------proxy------*/
+/*-----------------*/
+
+const dinner = {
+  meal: 'tacos',
+  test: 'proxy'
+}
+
+const handler = {
+  get (target, property, receiver) {
+    // console.log(target, property)
+    // return target[property]
+    console.log(arguments)
+    // track(target, property)
+    return Reflect.get(...arguments)
+  },
+
+  set (target, property, value, receiver) {
+    // console.log('set', target, property, value)
+    // target[property] = value
+    // return true
+    console.log(arguments)
+    // trigger(target, property)
+    return Reflect.set(...arguments)
+  }
+}
+
+const proxy = new Proxy(dinner, handler)
+console.log(proxy.meal)
+proxy.a = 'xxxx'
+console.log(proxy)
